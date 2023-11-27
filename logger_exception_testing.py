4caee8033326a86207b05c7c7e1cbd051d2017ad
ishawant kumar <ishawantkumar@gmail.com>
2023-11-27 11:17:23 +0530

    logger execption tested

---
+++ logger_exception_testing.py
@@ -0,0 +1,6 @@
+from src.mlProject import logger
+from src.mlProject.exception import CustomException 
+from src.mlProject.logger import logging 
+
+if __name__=='__main__':
+    logging.info('Checking logging')
