e37dcd18e1a7d49466484a3b54afeb48c7aab891
ishawant kumar <ishawantkumar@gmail.com>
2023-11-27 11:20:51 +0530

    exception checked

--- logger_exception_testing.py
+++ logger_exception_testing.py
@@ -1,6 +1,12 @@
 from src.mlProject import logger
 from src.mlProject.exception import CustomException 
 from src.mlProject.logger import logging 
+import sys 
 
 if __name__=='__main__':
-    logging.info('Checking logging')
+    logging.info('start logging')
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("logging completed")
+        raise CustomException(e,sys)
