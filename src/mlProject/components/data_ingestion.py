d6684082414e0d69259c2deefa3c98c2a48084f6
ishawant kumar <ishawantkumar@gmail.com>
2023-12-02 10:55:44 +0530

    data transformation updated

--- src/mlProject/components/data_ingestion.py
+++ src/mlProject/components/data_ingestion.py
@@ -7,6 +7,9 @@ import numpy as np
 from sklearn.model_selection import train_test_split 
 from dataclasses import dataclass 
 
+from src.mlProject.components.data_transformation import DataTransformation,DataTransformationConfig
+
+
 @dataclass
 class DataIngestionConfig: 
     train_data_path : str = os.path.join('artifacts','train.csv')
@@ -45,5 +48,9 @@ class DataIngestion:
         
 if __name__=='__main__':
     obj = DataIngestion()
-    obj.initiate_data_ingestion() 
+    train_data,test_data = obj.initiate_data_ingestion() 
+    
+    data_transformation = DataTransformation() 
+    data_transformation.initiate_data_transformation(train_data,test_data)
+    
     
