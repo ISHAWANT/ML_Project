614ced9f288bc27f1366a38a28d302b9a7765975
ishawant kumar <ishawantkumar@gmail.com>
2023-12-17 11:59:45 +0530

    prediction pipeline code updated

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask,request,render_template 
+import numpy as np 
+import pandas as pd 
+from sklearn.preprocessing import StandardScaler 
+
+application = Flask(__name__) 
+
+app = application 
+
+# Route for home page 
+@app.route('/') 
+def index():
+    return render_template('index.html')
+
+@app.route('/predictdata',methods = ['GET','POST']) 
+def predict_datapoint():
+    if request.method=='GET':
+        return render_template('home.index')
+    else:
+        data = Custom
